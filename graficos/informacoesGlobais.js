41befcd957b95d6daaf8c877ca535ddbd1158565
laurichadograu <126159388+laurichadograu@users.noreply.github.com>
2024-11-08 10:58:53 +0000

    hj

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,13 +8,12 @@ async function visualizarInformacoesGlobais() {
     const horas = parseInt(dados.tempo_medio)
     const porcentagemConectada = ((pessoasConectaas / pessoasNoMundo) * 100).tofFixed(2);
     const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas 
     e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma 
     rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
     console.log(paragrafo)
-
-    const container = document.getElementById('graico-container')
+    const container = document.getElementById('grafico-container')
     container.appendChild(paragrafo)
 }
 vizualizarInformaçõesGlobais()
