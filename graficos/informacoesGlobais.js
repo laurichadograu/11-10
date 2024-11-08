9b689598da9856cc094b142c07f66e2e66ecd682
laurichadograu <126159388+laurichadograu@users.noreply.github.com>
2024-11-08 11:40:20 +0000

    kk

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,10 +3,10 @@ const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glo
 async function visualizarInformacoesGlobais() {
     conts res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectaas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
-    const porcentagemConectada = ((pessoasConectaas / pessoasNoMundo) * 100).tofFixed(2);
+    const porcentagemConectada = ((pessoasConectaas / pessoasNoMundo) * 100).toFixed(2);
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas 
@@ -16,4 +16,5 @@ async function visualizarInformacoesGlobais() {
     const container = document.getElementById('grafico-container')
     container.appendChild(paragrafo)
 }
+
 vizualizarInformaçõesGlobais()
